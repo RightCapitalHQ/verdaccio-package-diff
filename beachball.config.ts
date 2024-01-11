81015aee5c9421e0a392293d970e7e4bccb338a3
jzhangdev <zhangjie9477@gmail.com>
2024-01-11 10:39:47 +0800

    ci: config github actions

---
+++ beachball.config.ts
@@ -0,0 +1,48 @@
+import type { BeachballConfig } from 'beachball';
+
+const config: BeachballConfig = {
+  access: 'public',
+  registry: 'https://registry.npmjs.org',
+  ignorePatterns: [
+    '.*ignore',
+    '.yamllint',
+    '.eslintrc.cjs',
+    '.pnpm-store/**',
+    '.vscode/**',
+    'prettier.config.cjs',
+    'pnpm-lock.yaml',
+  ],
+  changelog: {
+    customRenderers: {
+      // eslint-disable-next-line @typescript-eslint/require-await
+      async renderChangeTypeHeader(changeType, renderInfo) {
+        const changelogDate = renderInfo.newVersionChangelog.date
+          .toLocaleDateString('zh-CN')
+          .replace(/\//g, '-');
+        const heading =
+          changeType === 'major' || changeType === 'minor' ? '##' : '###';
+        return `${heading} [${
+          renderInfo.newVersionChangelog.version
+        }](https://github.com/RightCapitalHQ/verdaccio-package-diff/tree/${encodeURIComponent(
+          renderInfo.newVersionChangelog.tag,
+        )}) (${changelogDate})`;
+      },
+      // Original template: https://github.com/microsoft/beachball/blob/aefbc1ac37ee85961cc787133c827f1fd3925550/src/changelog/renderPackageChangelog.ts#L93
+      // eslint-disable-next-line @typescript-eslint/require-await
+      async renderEntry(entry) {
+        if (entry.author === 'beachball') {
+          return `- ${entry.comment}`;
+        }
+        // Imitate GitHub's commit format https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/autolinked-references-and-urls#commit-shas
+        return `- ${entry.comment} ([${entry.commit.substring(
+          0,
+          7,
+        )}](https://github.com/RightCapitalHQ/verdaccio-package-diff/commit/${
+          entry.commit
+        }))`;
+      },
+    },
+  },
+};
+
+export default config;
